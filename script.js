339d249b55acdd539faea5a7fa3e837101073d3d
fabianofrank <fabianofrank@protonmail.com>
2021-11-22 14:43:36 -0300

    Add html structure and started js

---
+++ script.js
@@ -0,0 +1,16 @@
+const collection = [
+  {
+    title: ;
+    author: ;
+  }
+];
+
+function addBook() {
+  this.title = title;
+}
+
+const addBtn = document.querySelector('.add-btn');
+
+addBtn.addEventListener('click', () => {
+  
+})
