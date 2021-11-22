72bc3743fffb7fcc992880ce6fa4f3db7da15cbf
mre55 <misganawmihreteab>
2021-11-22 23:43:37 +0300

    Add dynamicaly the HTML to the page

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 // const bookAuthor = document.querySelector('.author')
 // const addBtn = document.querySelector('.add-btn');
 
+const listBooks = document.querySelector('.list-book');
+
 const collection = [];
 
 function addBook(title, author) {
@@ -12,11 +14,39 @@ function addBook(title, author) {
     });
     
     collection.push(book);
-    console.log(book);
+    console.log('', book);
 }
 
 addBook('hahah', 'okaay');
-console.log(collection);
+addBook('hahah', 'okaay');
+addBook('hahah', 'okaay');
+console.log('collection is ', collection);
+
+document.addEventListener('DOMContentLoaded', () => {
+    listBooks.innerHTML = collection.map((col, obj) => `
+    <div class="${obj}">
+        <p>${col.title}</p>
+        <p>${col.author}</p>
+        <button class="btn${obj}">Remove</button>
+    </div>
+    `)
+})
+
+function removeBook() {
+    for (let i = 0; i <= collection.length; i++) {
+        const removeBtn = document.querySelector('.btn'+i)
+        removeBtn.addEventListener('click', () => {
+            i.style.display = 'none'
+        })
+ }
+}
+
+
+
+
+
+
+
 
 // addBtn.addEventListener('click', (event) => {
 //   event.preventDefault();
